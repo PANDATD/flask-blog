94299ecd3b90dbbe75324dc43b8b050b6d2524cf
Tejas Dixit <tejasdixit17@gmail.com>
2021-07-16 10:20:14 +0530

    added more posts

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
-"""
+'''
 Import package or module area
-"""
+'''
 from flask import (
     Flask,
     render_template,
 )
 
-# Creating instance of Flask class
+# Creating instance of Flask class 
 app = Flask(__name__)
 
+posts = [
+    {"name":"Tejas Dixit",
+     "content":"Inside templates you also have access to the config,\
+      request, session and g 1 objects as well as the url_for() and get_flashed_messages() functions.",
+      "date":"17 nov 2001"},
+      {"name":"Vignesh Gawali",
+     "content":"Inside templates you also have access to the config,\
+      request, session and g 1 objects as well as the url_for() and get_flashed_messages() functions.",
+      "date":"18 nov 2001"},
+      {"name":"Datta kale",
+     "content":"Inside templates you also have access to the config,\
+      request, session and g 1 objects as well as the url_for() and get_flashed_messages() functions.",
+      "date":"19 nov 2001"},
+      {"name":"Kunal jagate",
+     "content":"Inside templates you also have access to the config,\
+      request, session and g 1 objects as well as the url_for() and get_flashed_messages() functions.",
+      "date":"18 nov 2001"},      
+]
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def index():
-    return render_template("index.html")
-
+    return render_template('index.html', posts=posts)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost")
+    app.run(debug=True,)
