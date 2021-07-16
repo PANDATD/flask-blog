bd7cce73f32a2bc8dd91bf5c69f9ea192b88e7ad
Tejas Dixit <tejasdixit17@gmail.com>
2021-07-16 09:18:22 +0530

    updated index.html file

---
+++ app.py
@@ -0,0 +1,18 @@
+'''
+Import package or module area
+'''
+from flask import (
+    Flask,
+    render_template,
+)
+
+# Creating instance of Flask class 
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.run(debug=True, host='localhost')
